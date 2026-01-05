c7a920f8ddbeb35843a6590768b4ec496f223a27
aziz bedoui <beduiaziz18@gmail.com>
2026-01-05 10:21:48 +0100

    added power bi dashboard for the admin

--- greengate-admin/src/pages/dashboard/Dashboard.jsx
+++ greengate-admin/src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,37 @@
 const Dashboard = () => {
-  return <h1>Welcome to Admin Dashboard</h1>;
+  const finalUrl = "https://app.powerbi.com/reportEmbed?reportId=7abaa820-2fef-4c89-a737-da9ecf0817e4&autoAuth=true&embeddedDemo=true&filterPaneEnabled=false&navContentPaneEnabled=false&actionBarEnabled=false";
+
+  const containerStyle = {
+    width: '100%',
+    height: '83vh',      
+    overflow: 'hidden',  
+    position: 'relative',
+    borderRadius: '12px',
+    boxShadow: '0 4px 20px rgba(0,0,0,0.08)',
+    backgroundColor: '#fff'
+  };
+
+  const iframeStyle = {
+    position: 'absolute',
+    top: '-40px',       
+    left: '-15px',        
+    width: '100%', 
+    height: 'calc(100% + 46px)', 
+    border: 'none'
+  };
+
+  return (
+    <div >
+      <div style={containerStyle}>
+        <iframe 
+          title="greengate" 
+          src={finalUrl} 
+          style={iframeStyle}
+          allowFullScreen={true}>
+        </iframe>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
